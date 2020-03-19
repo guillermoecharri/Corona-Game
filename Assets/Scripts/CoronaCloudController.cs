d6738870e84f0c87d046bdd5b3109e7d616f78b3
guillermoecharri <47165084+guillermoecharri@users.noreply.github.com>
2020-03-19 03:06:17 -0400

    The game works now
    cool beans professional btw

--- Assets/Scripts/CoronaCloudController.cs
+++ Assets/Scripts/CoronaCloudController.cs
@@ -9,12 +9,23 @@ public class CoronaCloudController : MonoBehaviour
     [SerializeField] PlayerHealth playerHealth;
     [SerializeField] float dps = 10;
     [SerializeField] Score score;
+    [SerializeField] GameObject[] platforms;
+    private float platTracker = 0;
+    private static int platformBufferSize = 12; //all the platform layers loaded at once
+    private GameObject[] activePlatforms = new GameObject[platformBufferSize];
     private bool playerIsInCloud;
 
     // Start is called before the first frame update
     void Start()
     {
         playerIsInCloud = false;
+
+        for(int i = 0; i < platformBufferSize; i++)
+        {
+            Vector3 pos = gameObject.transform.position;
+            pos.y -= i + 1;
+            activePlatforms[i] = Instantiate(platforms[Random.Range(0, platforms.Length)], pos, Quaternion.identity);
+        }
     }
 
     private void Update()
@@ -35,7 +46,26 @@ public class CoronaCloudController : MonoBehaviour
             cloud.transform.position = new Vector3(0, cloud.transform.position.y - movement, 0);
             //add to score
             score.Add(movement);
+            //increment platTracker
+            platTracker += movement;
+            if(platTracker >= 1)
+            {
+                platTracker -= 1;
+                UpdatePlatforms();
+            }
         } 
+    }
+
+    private void UpdatePlatforms()
+    {
+        Destroy(activePlatforms[0]);
+        for(int i = 0; i < platformBufferSize - 1; i++)
+        {
+            activePlatforms[i] = activePlatforms[i + 1];
+        }
+        Vector3 pos = gameObject.transform.position;
+        pos.y -= platformBufferSize;
+        Instantiate(platforms[Random.Range(0, platforms.Length)], pos, Quaternion.identity);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
