a9eb9927f8a3938e2f5bc3b9a87e228b61b58d20
guillermoecharri <47165084+guillermoecharri@users.noreply.github.com>
2020-03-18 22:58:35 -0400

    Added Character Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float horizontalMovement;
+    [SerializeField] float speed = 1;
+    [SerializeField] GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.touchCount > 0)
+        {
+            Vector3 touchesPosition = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+
+            if (touchesPosition.x < 0) //move left
+            {
+                player.transform.position -= new Vector3((speed * Time.deltaTime), 0, 0);
+            }
+            else //move right
+            {
+                player.transform.position += new Vector3((speed * Time.deltaTime), 0, 0);
+            }
+        }
+    }
+}
