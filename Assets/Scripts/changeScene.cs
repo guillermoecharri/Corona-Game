5eadb7252ca0262d845604025376c104e8cc0fd4
Tristan Millman <tristanmillman@gmail.com>
2020-03-31 02:14:41 -0400

    basic menu

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class changeScene : MonoBehaviour
+{
+   
+    public void loadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
