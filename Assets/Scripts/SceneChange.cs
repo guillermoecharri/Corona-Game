7520237467954d84c84a4a641f2cba70fedecbc3
guillermoecharri <47165084+guillermoecharri@users.noreply.github.com>
2020-04-13 03:18:02 -0400

    Replaced ChangeScene with SceneChange

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class ChangeScene : MonoBehaviour
+public class SceneChange : MonoBehaviour
 {
 
     public void LoadScene(string scene)
