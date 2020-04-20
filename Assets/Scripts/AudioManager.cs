16f7058cb7b81873bd2ec41e923f1f3b7512a66f
guillermoecharri <47165084+guillermoecharri@users.noreply.github.com>
2020-04-20 19:13:29 -0400

    Created an AudioManager and Settings Panel
    Created an AudioManager to handle all the sounds in the app.
    Added the main theme to the audio manager and set it to loop while playing.
    Created a settings panel that can be accessed through the settings button in the main menu.
    Volume for all "UI", "Game", and "Music" sounds can be adjusted in that panel.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private Sound[] sounds;
+    private static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //use singleton pattern to ensure there is only one audio manager
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject); //allows AudioManager to persist between scenes to prevent breaks in looped tracks
+
+        //set up audio sources for all sounds
+        for(int i = 0; i < sounds.Length; i++)
+        {
+            sounds[i].SetAudioSource(gameObject.AddComponent<AudioSource>());
+            sounds[i].SetupAudioSource();
+        }
+
+        //play the main theme
+        Play("Main Theme");
+    }
+
+    public void Play (string name)
+    {
+        //find the sound
+        Sound sound = null;
+        for(int i = 0; i < sounds.Length; i++)
+        {
+            if(sounds[i].GetName() == name)
+            {
+                sound = sounds[i];
+            }
+        }
+
+        //ensure the sound exist
+        if(sound == null)
+        {
+            Debug.LogWarning("Sound with name: " + name + " not found.");
+        }
+        else //play the sound
+        {
+            sound.GetAudioSource().Play();
+        }
+    }
+
+    public void AdjustVolume (SoundType soundType, float volume)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].GetSoundType() == soundType)
+            {
+                sounds[i].ChangeVolume(volume);
+            }
+        }
+    }
+
+    public void AdjustVolumeUI (float volume) //work around function since unity buttons cannot pass enums or multiple arguments
+    {
+        AdjustVolume(SoundType.UI, volume);
+    }
+
+    public void AdjustVolumeMusic (float volume)
+    {
+        AdjustVolume(SoundType.Music, volume);
+    }
+
+    public void AdjustVolumeGame(float volume)
+    {
+        AdjustVolume(SoundType.Game, volume);
+    }
+
+    public void Mute(SoundType soundType)
+    {
+        AdjustVolume(soundType, 0f);
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable] public enum SoundType { UI, Music, Game }
+
+[System.Serializable]
+public class Sound
+{
+    [SerializeField] private string name;
+    
+    [SerializeField] SoundType soundType;
+    [SerializeField] private AudioClip audioClip;
+    [SerializeField] [Range(0f, 1f)] private float modifierVolume; //used to adjust default volume of the clip (incase its too loud/soft)
+    [SerializeField] [Range(0f, 1f)] private float userVolume = 0.5f; //users set volume for the type of clip
+    [SerializeField] private bool loop;
+    private AudioSource audioSource;
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public SoundType GetSoundType()
+    {
+        return soundType;
+    }
+
+    public AudioClip GetAudioClip()
+    {
+        return audioClip;
+    }
+
+    public void SetAudioSource(AudioSource audioSource)
+    {
+        this.audioSource = audioSource;
+    }
+
+    public AudioSource GetAudioSource()
+    {
+        return audioSource;
+    }
+
+    public void SetupAudioSource()
+    {
+        audioSource.clip = audioClip;
+        audioSource.volume = modifierVolume * userVolume;
+        audioSource.loop = loop;
+    }
+
+    public void ChangeVolume(float userVolume)
+    {
+        audioSource.volume = modifierVolume * userVolume;
+    }
+}
