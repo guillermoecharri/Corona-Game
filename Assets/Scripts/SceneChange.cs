bac89d3e7a824d891b74361e8f9dce8d822aa8ab
Tristan Millman <tristanmillman@gmail.com>
2020-04-21 02:51:25 -0400

    Transitions Done

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 
 public class SceneChange : MonoBehaviour
 {
+
+    public Animator transition;
+    public float transitionTime = 0.5f;
+
     public void LoadScene(string scene)
     {
         //change scene
+        StartCoroutine(LoadLevel(scene));
+    }
+
+    IEnumerator LoadLevel(string scene)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
         SceneManager.LoadScene(scene);
         Time.timeScale = 1f;
     }
